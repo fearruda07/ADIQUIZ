6c30136c62af52fef55611cac4c8288ec7802177
Arruda <Felipe.Arruda@externals.adidas.com>
2023-01-07 15:26:58 -0300

    AdiQuiz

---
+++ script.js
@@ -0,0 +1,55 @@
+
+const quiz =[
+    {	
+        q:'Em 1970 surge o icone superstar o primeiro tenis com cano baixo inicialmente feito pra as quadras de basquete se tornou viral atraves do jogador....',
+        options:['Bill Russel','Abdul-Jabbar','Stephen Curry'],
+        answer:1
+    },
+    {
+        q:'O EMMNXT tem como objetivo transformar a jornada do consumidor em loja.....',
+        options:['Mais objetiva','O conectando,engajando e inspirando','Através da técnica F.A.B','Mais divertida'],
+        answer:1
+    },
+    {	
+        q:'O programa especialista running e women foi criado com intuito de...',
+        options:['Diminuir as Conversão nas lojas','Diminuir o turnover','Trazer colaboradores mais capacitados em lojas'],
+        answer:2
+    },
+    {	
+        q:'O tenis 4dfwd tem uma tecnologia que gera retorno de energia te impulsionando para..',
+        options:['Cima economizando passos','Ambos os lados','Frente economizando passos','Para trás reduzindo passos'],
+        answer:2
+    },
+    {	
+        q:'Em que ano nasce oficialmente a Adidas? e em que cidade ela foi instalada inicialmente?',
+        options:['1948 em Oberhausen','1949 em Herzogenarauch','1949 em Lyon (Rhône-Alpes)','1951 em Hanôver'],
+        answer:1
+    },
+    {	
+        q:"Em 2022 abriu uma loja conceito com 700m em formato stadium na AV.paulista que loja foi essa?",
+        options:['City Center','Top Center','Nomura center','Bourbon center'],
+        answer:1
+    },
+    {	
+        q:"Qual objetivo do programa in store coach?",
+        options:["Ter maior produtividade e rentabilidade junto ao time de vendas","Conhecer os produtos","Aprender sobre a cultura da adidas somente","Aumentar o turnover"],
+        answer:0
+    },
+    {
+        q:"Na campanha de incentivo VENDE QUE É GOL tivemos 15 vencedores de lojas BCS, que loja ficou em 1° lugar?",
+        options:["BCS- Rio Branco,Via Verde","BCS- Rio Porto-Alegre","BCS- Brasilia, Iguatemi","BCS- Foz do Iguaçu,Cataratass"],
+        answer:0
+    },
+    {
+        q:"Sabemos que o jeans utiliza muita água em sua confecção, quantos litros é utilizado para confecção de 2 peças de jeans?",
+        options:["500 a 800 litros","1000 litros","6.800 a 8.000 litros"],
+        answer:2
+    },
+    {
+        q:"Qual evento que temos em Q2 e Q4",
+        options:["EMMNXT","Key players","Bootcamp"],
+        answer:2
+    },
+    ]
+    
+    
